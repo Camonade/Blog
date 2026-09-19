6a046d145d74d29f879a5f141d08f071285fddd6
Camonade <20061125gqy@gmail>
2026-09-20 02:37:53 +0800

    站点域名改为 camonade-blog.camonade.workers.dev

--- blog.config.ts
+++ blog.config.ts
@@ -20,7 +20,7 @@ const basicConfig = {
 	language: 'zh-CN',
 	timeEstablished: '2026-09-16',
 	timeZone: 'Asia/Shanghai',
-	url: 'https://camonade-blog.20061125gqy.workers.dev/',
+	url: 'https://camonade-blog.camonade.workers.dev/',
 	defaultCategory: '未分类',
 }
 
